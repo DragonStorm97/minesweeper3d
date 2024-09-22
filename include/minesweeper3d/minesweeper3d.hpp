1a00eceac18f3eda632afa572af940e947d84c2a
Johnathan Jacobs <jonathan.jacobs1997@gmail.com>
2024-09-22 17:18:34 +0200

    init generated repo

---
+++ include/minesweeper3d/minesweeper3d.hpp
@@ -0,0 +1,2 @@
+// The main include files for this library minesweeper3d
+#pragma once
---
+++ minesweeper3d/library/src/libminesweeper3d.cpp
@@ -0,0 +1,8 @@
+#include "minesweeper3d.hpp"
+#include "libminesweeper3d.hpp"
+
+int libfunc()
+{
+  return coolNumberBro;
+}
+
